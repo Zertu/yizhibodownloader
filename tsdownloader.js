faa1a29dffe9084fddb2af4f80bbeba5d1f2d9ae
zertu <290055513@qq.com>
2017-10-22 11:20:33 +0800

    检查文件是否存在，存在的不用下载，伪断点续传

--- tsdownloader.js
+++ tsdownloader.js
@@ -1,19 +1,34 @@
 const fs = require('fs'),
-    download = require('download')
-    ,ProgressBar = require('progress')
+    download = require('download'),
+    ProgressBar = require('progress'),
+    path=require('path')
 module.exports = function downloader(baseUrl, tags) {
-    let lists = []    
-    bar = new ProgressBar(':bar', { total: tags.length })
+    let lists = []
+    bar = new ProgressBar(':bar', {total: tags.length})
     tags.map(list => lists.push(new Promise(async(resolve, reject) => {
-        try{
-            const data = await download(baseUrl +'/'+ list)
-            bar.tick()
-            console.info(list+'下载完毕')
-            resolve(fs.writeFileSync(list,data))
-        }
-        catch (e){
-            reject(e)
+        let temp =await fileExists(list)
+        if (!temp) {
+            resolve(console.info(list + '已存在'))
+        } else {
+            try {
+                const data = await download(baseUrl + '/' + list,{
+                    timeout:60000
+                })
+                bar.tick()
+                console.info(list + '下载完毕')
+                resolve(fs.writeFileSync(list, data))
+            } catch (e) {
+                reject(e)
+            }
         }
     })))
     return Promise.all(lists)
+}
+
+function fileExists(filename) {
+    return new Promise((resolve, reject) => {
+        fs.exists(__dirname+filename, (exists) => {
+            resolve(exists)
+        })
+    })
 }
